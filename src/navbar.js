57666be4289a6f07dff975c4206dd67d1c10111e
jake612 <jake612@live.unc.edu>
2020-05-16 21:11:17 -0400

    Changed css to look nicer and added additional info in header

--- src/navbar.js
+++ src/navbar.js
@@ -3,9 +3,14 @@ import React, {Component} from 'react';
 export class Navbar extends Component {
     render(){
         return <div className="navbar">
-            <button className="nav_button" onClick={this.props.addNode}>Add</button>
-            <button className="nav_button" onClick={this.props.deleteNode}>Delete</button>
-            <button className="nav_button" onClick={this.props.nextRank}>Next</button>
+            <div className="nav_section">
+                <div className="button_wrapper"><button className="nav_button" onClick={this.props.addNode}>Add</button></div>
+                <div className="button_wrapper"><button className="nav_button" onClick={this.props.deleteNode}>Delete</button></div>
+            </div>
+            <div className="divider" />
+            <div className="nav_section">
+                <div className="button_wrapper"><button className="nav_button" onClick={this.props.nextRank}>Next</button></div>
+            </div>
         </div>
     }
 }
